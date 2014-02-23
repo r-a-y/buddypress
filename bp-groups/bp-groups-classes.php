r-a-y/buddypress

commit 22381463e21e3422ee384cb1881c749ba94ad17a

Overhaul the way that individual group objects are cached
The existing implementation was broken in a number of ways. Most critically,
user-specific data (like is_member) was being stored in the persistent object
cache without respect to user.

This refactor excludes the user-specific data from being cached along with the
group object. It also reworks the way that items are keyed in the cache, for
greater consistency with WordPress and the other BP components.

See #5407

git-svn-id: http://buddypress.svn.wordpress.org/trunk@7956 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-groups/bp-groups-classes.php
@@ -176,22 +176,49 @@ class BP_Groups_Group {
 	public function populate() {
 		global $wpdb, $bp;
 
-		if ( $group = $wpdb->get_row( $wpdb->prepare( "SELECT g.* FROM {$bp->groups->table_name} g WHERE g.id = %d", $this->id ) ) ) {
-			$this->id                 = $group->id;
-			$this->creator_id         = $group->creator_id;
-			$this->name               = stripslashes($group->name);
-			$this->slug               = $group->slug;
-			$this->description        = stripslashes($group->description);
-			$this->status             = $group->status;
-			$this->enable_forum       = $group->enable_forum;
-			$this->date_created       = $group->date_created;
+		$group = wp_cache_get( $this->id, 'bp_groups' );
+		if ( false === $group ) {
+			$group = $wpdb->get_row( $wpdb->prepare( "SELECT g.* FROM {$bp->groups->table_name} g WHERE g.id = %d", $this->id ) );
+
+			if ( empty( $group ) ) {
+				$this->id = 0;
+				return;
+			}
+
+			$this->id           = $group->id;
+			$this->creator_id   = $group->creator_id;
+			$this->name         = stripslashes( $group->name );
+			$this->slug         = $group->slug;
+			$this->description  = stripslashes( $group->description );
+			$this->status       = $group->status;
+			$this->enable_forum = $group->enable_forum;
+			$this->date_created = $group->date_created;
 
 			if ( ! empty( $this->args['populate_extras'] ) ) {
 				$this->last_activity      = groups_get_groupmeta( $this->id, 'last_activity' );
 				$this->total_member_count = groups_get_groupmeta( $this->id, 'total_member_count' );
-				$this->is_member          = BP_Groups_Member::check_is_member( bp_loggedin_user_id(), $this->id );
-				$this->is_invited         = BP_Groups_Member::check_has_invite( bp_loggedin_user_id(), $this->id );
-				$this->is_pending         = BP_Groups_Member::check_for_membership_request( bp_loggedin_user_id(), $this->id );
+
+				// Get group admins and mods
+				$admin_mods = $wpdb->get_results( apply_filters( 'bp_group_admin_mods_user_join_filter', $wpdb->prepare( "SELECT u.ID as user_id, u.user_login, u.user_email, u.user_nicename, m.is_admin, m.is_mod FROM {$wpdb->users} u, {$bp->groups->table_name_members} m WHERE u.ID = m.user_id AND m.group_id = %d AND ( m.is_admin = 1 OR m.is_mod = 1 )", $this->id ) ) );
+
+				foreach ( (array) $admin_mods as $user ) {
+					if ( (int) $user->is_admin ) {
+						$this->admins[] = $user;
+					} else {
+						$this->mods[] = $user;
+					}
+				}
+
+				// Cache general (non-user-specific)
+				// information about the group
+				wp_cache_set( $this->id, $this, 'bp_groups' );
+			}
+
+			// Set user-specific data
+			if ( ! empty( $this->args['populate_extras'] ) ) {
+				$this->is_member  = BP_Groups_Member::check_is_member( bp_loggedin_user_id(), $this->id );
+				$this->is_invited = BP_Groups_Member::check_has_invite( bp_loggedin_user_id(), $this->id );
+				$this->is_pending = BP_Groups_Member::check_for_membership_request( bp_loggedin_user_id(), $this->id );
 
 				// If this is a private or hidden group, does the current user have access?
 				if ( 'private' == $this->status || 'hidden' == $this->status ) {
@@ -202,18 +229,7 @@ class BP_Groups_Group {
 				} else {
 					$this->user_has_access = true;
 				}
-
-				// Get group admins and mods
-				$admin_mods = $wpdb->get_results( apply_filters( 'bp_group_admin_mods_user_join_filter', $wpdb->prepare( "SELECT u.ID as user_id, u.user_login, u.user_email, u.user_nicename, m.is_admin, m.is_mod FROM {$wpdb->users} u, {$bp->groups->table_name_members} m WHERE u.ID = m.user_id AND m.group_id = %d AND ( m.is_admin = 1 OR m.is_mod = 1 )", $this->id ) ) );
-				foreach( (array) $admin_mods as $user ) {
-					if ( (int) $user->is_admin )
-						$this->admins[] = $user;
-					else
-						$this->mods[] = $user;
-				}
 			}
-		} else {
-			$this->id = 0;
 		}
 	}
 
@@ -288,7 +304,7 @@ class BP_Groups_Group {
 
 		do_action_ref_array( 'groups_group_after_save', array( &$this ) );
 
-		wp_cache_delete( 'bp_groups_group_' . $this->id, 'bp' );
+		wp_cache_delete( $this->id, 'bp_groups' );
 
 		return true;
 	}
@@ -316,7 +332,7 @@ class BP_Groups_Group {
 
 		do_action_ref_array( 'bp_groups_delete_group', array( &$this, $user_ids ) );
 
-		wp_cache_delete( 'bp_groups_group_' . $this->id, 'bp' );
+		wp_cache_delete( $this->id, 'bp_groups' );
 
 		// Finally remove the group entry from the DB
 		if ( !$wpdb->query( $wpdb->prepare( "DELETE FROM {$bp->groups->table_name} WHERE id = %d", $this->id ) ) )

bp-groups/bp-groups-cache.php
@@ -64,8 +64,7 @@ add_action( 'groups_create_group_step_complete', 'groups_clear_group_object_cach
  * @param int $group_id The group being edited.
  */
 function bp_groups_delete_group_cache( $group_id = 0 ) {
-	wp_cache_delete( 'bp_groups_group_' . $group_id . '_load_users', 'bp' );
-	wp_cache_delete( 'bp_groups_group_' . $group_id . '_noload_users', 'bp' );
+	wp_cache_delete( $group_id, 'bp_groups' );
 }
 add_action( 'groups_delete_group',     'bp_groups_delete_group_cache' );
 add_action( 'groups_update_group',     'bp_groups_delete_group_cache' );
@@ -78,8 +77,7 @@ add_action( 'groups_settings_updated', 'bp_groups_delete_group_cache' );
  * @since BuddyPress (2.0.0)
  */
 function bp_groups_delete_group_cache_on_metadata_change( $meta_id, $group_id ) {
-	wp_cache_delete( 'bp_groups_group_' . $group_id . '_load_users', 'bp' );
-	wp_cache_delete( 'bp_groups_group_' . $group_id . '_noload_users', 'bp' );
+	wp_cache_delete( $group_id, 'bp_groups' );
 }
 add_action( 'updated_group_meta', 'bp_groups_delete_group_cache_on_metadata_change', 10, 2 );
 add_action( 'added_group_meta', 'bp_groups_delete_group_cache_on_metadata_change', 10, 2 );

bp-groups/bp-groups-functions.php
@@ -40,25 +40,17 @@ function bp_groups_has_directory() {
  * @return BP_Groups_Group $group The group object
  */
 function groups_get_group( $args = '' ) {
-	$defaults = array(
+	$r = wp_parse_args( $args, array(
 		'group_id'          => false,
 		'load_users'        => false,
 		'populate_extras'   => true,
+	) );
+
+	$group_args = array(
+		'populate_extras' => $r['populate_extras'],
 	);
 
-	$args = wp_parse_args( $args, $defaults );
-	extract( $args, EXTR_SKIP );
-
-	$cache_key = 'bp_groups_group_' . $group_id . ( $load_users ? '_load_users' : '_noload_users' );
-
-	if ( !$group = wp_cache_get( $cache_key, 'bp' ) ) {
-		$group_args = array(
-			'populate_extras'   => $populate_extras,
-		);
-
-		$group = new BP_Groups_Group( $group_id, $group_args );
-		wp_cache_set( $cache_key, $group, 'bp' );
-	}
+	$group = new BP_Groups_Group( $r['group_id'], $group_args );
 
 	return apply_filters( 'groups_get_group', $group );
 }

tests/testcases/groups/cache.php
@@ -73,14 +73,14 @@ class BP_Tests_Group_Cache extends BP_UnitTestCase {
 		$g = $this->factory->group->create();
 
 		// Prime cache
-		groups_get_group( array( 'group_id' => $g ) );
+		groups_get_group( array( 'group_id' => $g, ) );
 
-		$this->assertNotEmpty( wp_cache_get( 'bp_groups_group_' . $g . '_noload_users', 'bp' ) );
+		$this->assertNotEmpty( wp_cache_get( $g, 'bp_groups' ) );
 
 		// Trigger flush
 		groups_update_groupmeta( $g, 'foo', 'bar' );
 
-		$this->assertFalse( wp_cache_get( 'bp_groups_group_' . $g . '_noload_users', 'bp' ) );
+		$this->assertFalse( wp_cache_get( $g, 'bp_groups' ) );
 	}
 
 	/**
@@ -94,11 +94,11 @@ class BP_Tests_Group_Cache extends BP_UnitTestCase {
 		groups_update_groupmeta( $g, 'foo', 'bar' );
 		groups_get_group( array( 'group_id' => $g ) );
 
-		$this->assertNotEmpty( wp_cache_get( 'bp_groups_group_' . $g . '_noload_users', 'bp' ) );
+		$this->assertNotEmpty( wp_cache_get( $g, 'bp_groups' ) );
 
 		// Trigger flush
 		groups_update_groupmeta( $g, 'foo', 'baz' );
-		$this->assertFalse( wp_cache_get( 'bp_groups_group_' . $g . '_noload_users', 'bp' ) );
+		$this->assertFalse( wp_cache_get( $g, 'bp_groups' ) );
 	}
 
 }

tests/testcases/groups/class-bp-groups-group.php
@@ -643,6 +643,42 @@ class BP_Tests_BP_Groups_Group_TestCases extends BP_UnitTestCase {
 	}
 
 	/**
+	 * @group delete
+	 * @group cache
+	 */
+	public function test_delete_clear_cache() {
+		$g = $this->factory->group->create();
+
+		// Prime cache
+		groups_get_group( array( 'group_id' => $g, ) );
+
+		$this->assertNotEmpty( wp_cache_get( $g, 'bp_groups' ) );
+
+		$group = new BP_Groups_Group( $g );
+		$group->delete();
+
+		$this->assertFalse( wp_cache_get( $g, 'bp_groups' ) );
+	}
+
+	/**
+	 * @group save
+	 * @group cache
+	 */
+	public function test_save_clear_cache() {
+		$g = $this->factory->group->create();
+
+		// Prime cache
+		groups_get_group( array( 'group_id' => $g, ) );
+
+		$this->assertNotEmpty( wp_cache_get( $g, 'bp_groups' ) );
+
+		$group = new BP_Groups_Group( $g );
+		$group->name = 'Foo';
+		$group->save();
+
+		$this->assertFalse( wp_cache_get( $g, 'bp_groups' ) );
+	}
+	/**
 	 * @group get_group_extras
 	 */
 	public function test_get_group_extras_non_logged_in() {

tests/testcases/groups/functions.php
@@ -548,4 +548,28 @@ Bar!';
 		groups_add_groupmeta( $g, 'foo', 'bar' );
 		$this->assertNotEmpty( groups_add_groupmeta( $g, 'foo', 'baz' ) );
 	}
+
+	/**
+	 * @group groups_get_group
+	 * @group cache
+	 */
+	public function test_groups_get_group_cache_different_users() {
+		$g = $this->factory->group->create();
+		$u1 = $this->create_user();
+		$u2 = $this->create_user();
+		$this->add_user_to_group( $u1, $g );
+
+		$old_user = get_current_user_id();
+		$this->set_current_user( $u1 );
+
+		$group1 = groups_get_group( array( 'group_id' => $g, ) );
+
+		$this->set_current_user( $u2 );
+
+		$group2 = groups_get_group( array( 'group_id' => $g, ) );
+
+		$this->assertNotEquals( $group1, $group2 );
+
+		$this->set_current_user( $old_user );
+	}
 }

commit 252e1a933f0ec91df3d0ab6f07fa36ece74e52c5

Fix logic error in caching in BP_Groups_Group::populate()
Introduced in r7956, this bug cause the populate logic not to be run when
the cache was hit successfully. Mega-anti-props boonebgorges.

See #5407

git-svn-id: http://buddypress.svn.wordpress.org/trunk@7974 cdf35c40-ae34-48e0-9cc9-0c9da1808c22

bp-groups/bp-groups-classes.php
@@ -179,56 +179,56 @@ class BP_Groups_Group {
 		$group = wp_cache_get( $this->id, 'bp_groups' );
 		if ( false === $group ) {
 			$group = $wpdb->get_row( $wpdb->prepare( "SELECT g.* FROM {$bp->groups->table_name} g WHERE g.id = %d", $this->id ) );
+		}
 
-			if ( empty( $group ) ) {
-				$this->id = 0;
-				return;
-			}
+		if ( empty( $group ) ) {
+			$this->id = 0;
+			return;
+		}
 
-			$this->id           = $group->id;
-			$this->creator_id   = $group->creator_id;
-			$this->name         = stripslashes( $group->name );
-			$this->slug         = $group->slug;
-			$this->description  = stripslashes( $group->description );
-			$this->status       = $group->status;
-			$this->enable_forum = $group->enable_forum;
-			$this->date_created = $group->date_created;
+		$this->id           = $group->id;
+		$this->creator_id   = $group->creator_id;
+		$this->name         = stripslashes( $group->name );
+		$this->slug         = $group->slug;
+		$this->description  = stripslashes( $group->description );
+		$this->status       = $group->status;
+		$this->enable_forum = $group->enable_forum;
+		$this->date_created = $group->date_created;
 
-			if ( ! empty( $this->args['populate_extras'] ) ) {
-				$this->last_activity      = groups_get_groupmeta( $this->id, 'last_activity' );
-				$this->total_member_count = groups_get_groupmeta( $this->id, 'total_member_count' );
+		if ( ! empty( $this->args['populate_extras'] ) ) {
+			$this->last_activity      = groups_get_groupmeta( $this->id, 'last_activity' );
+			$this->total_member_count = groups_get_groupmeta( $this->id, 'total_member_count' );
 
-				// Get group admins and mods
-				$admin_mods = $wpdb->get_results( apply_filters( 'bp_group_admin_mods_user_join_filter', $wpdb->prepare( "SELECT u.ID as user_id, u.user_login, u.user_email, u.user_nicename, m.is_admin, m.is_mod FROM {$wpdb->users} u, {$bp->groups->table_name_members} m WHERE u.ID = m.user_id AND m.group_id = %d AND ( m.is_admin = 1 OR m.is_mod = 1 )", $this->id ) ) );
+			// Get group admins and mods
+			$admin_mods = $wpdb->get_results( apply_filters( 'bp_group_admin_mods_user_join_filter', $wpdb->prepare( "SELECT u.ID as user_id, u.user_login, u.user_email, u.user_nicename, m.is_admin, m.is_mod FROM {$wpdb->users} u, {$bp->groups->table_name_members} m WHERE u.ID = m.user_id AND m.group_id = %d AND ( m.is_admin = 1 OR m.is_mod = 1 )", $this->id ) ) );
 
-				foreach ( (array) $admin_mods as $user ) {
-					if ( (int) $user->is_admin ) {
-						$this->admins[] = $user;
-					} else {
-						$this->mods[] = $user;
-					}
-				}
-
-				// Cache general (non-user-specific)
-				// information about the group
-				wp_cache_set( $this->id, $this, 'bp_groups' );
-			}
-
-			// Set user-specific data
-			if ( ! empty( $this->args['populate_extras'] ) ) {
-				$this->is_member  = BP_Groups_Member::check_is_member( bp_loggedin_user_id(), $this->id );
-				$this->is_invited = BP_Groups_Member::check_has_invite( bp_loggedin_user_id(), $this->id );
-				$this->is_pending = BP_Groups_Member::check_for_membership_request( bp_loggedin_user_id(), $this->id );
-
-				// If this is a private or hidden group, does the current user have access?
-				if ( 'private' == $this->status || 'hidden' == $this->status ) {
-					if ( $this->is_member && is_user_logged_in() || bp_current_user_can( 'bp_moderate' ) )
-						$this->user_has_access = true;
-					else
-						$this->user_has_access = false;
+			foreach ( (array) $admin_mods as $user ) {
+				if ( (int) $user->is_admin ) {
+					$this->admins[] = $user;
 				} else {
-					$this->user_has_access = true;
+					$this->mods[] = $user;
 				}
+			}
+
+			// Cache general (non-user-specific)
+			// information about the group
+			wp_cache_set( $this->id, $this, 'bp_groups' );
+		}
+
+		// Set user-specific data
+		if ( ! empty( $this->args['populate_extras'] ) ) {
+			$this->is_member  = BP_Groups_Member::check_is_member( bp_loggedin_user_id(), $this->id );
+			$this->is_invited = BP_Groups_Member::check_has_invite( bp_loggedin_user_id(), $this->id );
+			$this->is_pending = BP_Groups_Member::check_for_membership_request( bp_loggedin_user_id(), $this->id );
+
+			// If this is a private or hidden group, does the current user have access?
+			if ( 'private' == $this->status || 'hidden' == $this->status ) {
+				if ( $this->is_member && is_user_logged_in() || bp_current_user_can( 'bp_moderate' ) )
+					$this->user_has_access = true;
+				else
+					$this->user_has_access = false;
+			} else {
+				$this->user_has_access = true;
 			}
 		}
 	}